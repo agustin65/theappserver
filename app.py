545459ab77a3b8cb1fee0a897d154931703e74dd
SoyYoCapo <agustinfabricius65@gmail.com>
2020-07-18 12:57:16 -0300

    actualizado , sigue con buggsssss

--- app.py
+++ app.py
@@ -2,42 +2,50 @@ import pyautogui
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-currentCursorX = 0
-currentCursorY = 0
-
 variable = 2
+
+X0 = -1
+Y0 = -1
+StartCoords = False
 
 app = Flask(__name__)
 
 CORS(app)
 
 
-@app.route('/start', methods=['POST'])
-def Start():
-    global currentCursorX
-    global currentCursorY
-    currentCursorX = request.json['x']
-    currentCursorY = request.json['y']
+@app.route('/api', methods=['POST'])
+def Api():
+    if(StartCoords):
+        Touches = request.json
+        if(len(Touches) == 1):
+            X = Touches[0]['x']
+            Y = Touches[0]['y']
+            if(0 < X and X < 1 and 0 < Y and Y < 1):
+                screenWidth, screenHeight = pyautogui.size()
+                currentMouseX, currentMouseY = pyautogui.position()
+                moveX = currentMouseX + ((X-X0) * screenWidth / variable)
+                moveY = currentMouseY + ((Y-Y0) * screenHeight / variable)
+                print(moveX)
+                pyautogui.moveTo(moveX, moveY)
+
     return 'ok'
 
+@app.route('/start',methods=['POST'])
+def Start():
+    global StartCoords
+    if not (StartCoords):
+        global X0
+        global Y0
+        X0 = request.json['x']
+        Y0 = request.json['y']
+        StartCoords = True
+    return 'ok'
 
 @app.route('/stop', methods=['POST'])
 def Stop():
-    Comparar(request.json)
+    global StartCoords
+    StartCoords = False
     return 'ok'
-
-
-def Comparar(Obj):
-    x = currentCursorX-Obj['x']
-    y = currentCursorY-Obj['y']
-    if(abs(y) < 0.1 and abs(x) < 0.1):
-        pyautogui.click()
-    else:
-        screenWidth, screenHeight = pyautogui.size()
-        currentMouseX, currentMouseY = pyautogui.position()
-        moveX = currentMouseX - (x * screenWidth / variable)
-        moveY = currentMouseY - (y * screenHeight / variable)
-        pyautogui.moveTo(moveX, moveY)
 
 
 if __name__ == "__main__":
