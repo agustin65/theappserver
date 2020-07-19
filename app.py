181c604a99a00f68b81095877fe6b871fecd1a16
SoyYoCapo <agustinfabricius65@gmail.com>
2020-07-19 12:10:41 -0300

    cambiada la api, ahora se hacen bien los movimientos por pantalla

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import pyautogui
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-variable = 2
+pyautogui.FAILSAFE = False
+
+variable = 3
 
 X0 = -1
 Y0 = -1
@@ -12,12 +14,42 @@ app = Flask(__name__)
 
 CORS(app)
 
+@app.route('/move', methods=['POST'])
+def Move():
+    X = request.json['x']
+    Y = request.json['y']
+    if(-1 < X and X < 1 and -1 < Y and Y < 1):
+        screenWidth, screenHeight = pyautogui.size()
+        currentMouseX, currentMouseY = pyautogui.position()
+        moveX = currentMouseX + (X * screenWidth / variable)
+        moveY = currentMouseY + (Y * screenHeight / variable)
+        if(0 > moveY):
+            moveY = 0
+        if(0 > moveX):
+            moveX = 0
+        if(moveY > screenHeight):
+            moveY = screenHeight-1
+        if(moveX > screenWidth):
+            moveX = screenWidth-1
+        pyautogui.moveTo(moveX,moveY)
+    return 'ok'
+
+@app.route('/click')
+def Click():
+    pyautogui.click()
+    return 'ok'
+
 
 @app.route('/api', methods=['POST'])
 def Api():
+    global StartCoords
+    global X0
+    global Y0
+    Touches = request.json
     if(StartCoords):
-        Touches = request.json
-        if(len(Touches) == 1):
+        if(len(Touches) == 0):
+            StartCoords = False
+        elif(len(Touches) == 1):
             X = Touches[0]['x']
             Y = Touches[0]['y']
             if(0 < X and X < 1 and 0 < Y and Y < 1):
@@ -25,27 +57,18 @@ def Api():
                 currentMouseX, currentMouseY = pyautogui.position()
                 moveX = currentMouseX + ((X-X0) * screenWidth / variable)
                 moveY = currentMouseY + ((Y-Y0) * screenHeight / variable)
-                print(moveX)
-                pyautogui.moveTo(moveX, moveY)
-
-    return 'ok'
-
-@app.route('/start',methods=['POST'])
-def Start():
-    global StartCoords
-    if not (StartCoords):
-        global X0
-        global Y0
-        X0 = request.json['x']
-        Y0 = request.json['y']
+                if(0 < moveY and moveY < screenHeight and 0 < moveX and moveX < screenWidth ):
+                    pyautogui.moveTo(moveX, moveY)
+                elif(0 < moveY and moveY < screenHeight):
+                    pyautogui.moveTo(0, moveY)
+                elif(0 < moveX and moveX < screenWidth):
+                    pyautogui.moveTo(moveX, 0)
+    else:
+        X0 = Touches[0]['x']
+        Y0 = Touches[0]['y']
         StartCoords = True
     return 'ok'
 
-@app.route('/stop', methods=['POST'])
-def Stop():
-    global StartCoords
-    StartCoords = False
-    return 'ok'
 
 
 if __name__ == "__main__":
